0d57c5e6e7b2802b71b43bf0fe8923394f81b99f
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-05-12 21:08:05 +0600

    Add if/else find a movie by place or title

--- bin/main.rb
+++ bin/main.rb
@@ -8,13 +8,12 @@ search_engine = SearchEngine.new
 validator = Validator.new
 
 
-
 puts '                            -----WELCOME------'
 puts 'Here you can find a film from TOP 250 IMBD ranking'
 puts ''
 puts 'You can do it either by TITLE or by PLACE in a ranking'
 puts ''
-print 'Choose ---1--- if you want to search by TITLE and ---2--- for searching by PLACE:  '
+print 'Choose ---1--- if you want to search by PLACE and ---2--- for searching by TITLE:  '
 
 search_type = gets.chomp
 
@@ -23,13 +22,18 @@ until validator.search_type_validator(search_type)
     search_type = gets.chomp
 end
 
+if validator.choice_validator(search_type)
+  print 'Please, type a place of movie in IMBD ranking: '
+  place_of_choice = gets.chomp.to_i  
+  puts search_engine.title_search(place_of_choice)
+else
+  print 'Please, type a title of movie in IMBD ranking: '
+  title_of_choice = gets.chomp
+  title_of_choice = search_engine.capitalize_words(title_of_choice)
+  puts search_engine.place_search(title_of_choice)
+end
 
 
-print 'Please, type a place of movie in IMBD ranking: '
 
-place_of_choice = gets.chomp.to_i
 
-puts search_engine.title_search(place_of_choice)
-
-print 'Please, type a place of movie in IMBD ranking: '
 
