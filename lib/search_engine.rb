5be0d6de950235dc753ad351201f89d9933331c7
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-05-12 15:47:00 +0600

    Create parsing page method

--- lib/search_engine.rb
+++ lib/search_engine.rb
@@ -3,17 +3,20 @@ require 'httparty'
 require 'byebug'
 
 class SearchEngine
-def initialize 
-   @movie_listings = Array.new
-end
+  def initialize 
+    @movie_listings = Array.new
+    @movies_with_places = Array.new
+  end
 
-  def parsing_page
+  def self.parsing_page
     url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page.body)
-    movie_listings = parsed_page.css('td.titleColumn')     
+    @movie_listings = parsed_page.css('td.titleColumn')     
   end  
   
+  
+
   
   byebug
   
