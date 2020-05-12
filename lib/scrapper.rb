6386335433c0b0e3c6ec499375a7b6bdb2df6d11
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-05-12 16:48:30 +0600

    Fix rubocop errors in Scrapper file

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -2,28 +2,27 @@ require 'nokogiri'
 require 'httparty'
 require 'byebug'
 
-class SearchEngine
-
+class Scrapper
   def parsing_page
     url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page.body)
-    movie_listings = parsed_page.css('td.titleColumn')
-  end  
-# byebug
+    parsed_page.css('td.titleColumn')
+  end
+
   def hash_creator
     movies_with_places = []
-    place_count = 0    
-    self.parsing_page.each do |movie_listing|
+    place_count = 0
+    parsing_page.each do |movie_listing|
       movie = {
-        title: movie_listing.css('a').text,
+        title: movie_listing.css('a').text.downcase,
         place: place_count += 1
       }
       movies_with_places << movie
     end
     movies_with_places
-  end  
+  end
 end
 
-new_object = SearchEngine.new
+new_object = Scrapper.new
 print new_object.hash_creator
