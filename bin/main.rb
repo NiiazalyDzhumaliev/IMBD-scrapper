28493eadffbc5ff7699e328f746414005fddb6d1
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-05-12 15:38:51 +0600

    Create SearchEngine class

--- bin/main.rb
+++ bin/main.rb
@@ -2,39 +2,39 @@ require 'nokogiri'
 require 'httparty'
 require 'byebug'
 
-puts '                                     -----WELCOME------'
-puts 'Here you can find a film from TOP 250 IMBD ranking'
-puts ''
-puts 'You can find it by TITLE or by PLACE in a ranking'
-puts ''
-print 'Choose 1 if you want search by TITLE and 2 for searching by PLACE:'
-search_type = gets.chomp
+# puts '                                     -----WELCOME------'
+# puts 'Here you can find a film from TOP 250 IMBD ranking'
+# puts ''
+# puts 'You can find it by TITLE or by PLACE in a ranking'
+# puts ''
+# print 'Choose 1 if you want search by TITLE and 2 for searching by PLACE:'
+# search_type = gets.chomp
 
-def empty_validator(title)
-  title == "" ? true : false
-end
-
-while empty_validator(search_type)
-  puts 'Please, enter the valid command'
-  search_type = gets.chomp
-end
-
-# def scraper
-#   url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
-#   unparsed_page = HTTParty.get(url)
-#   parsed_page = Nokogiri::HTML(unparsed_page.body)
-#   jobs = Array.new
-#   job_listings = parsed_page.css('td.titleColumn')
-#   count = 0
-#   job_listings.each do |job_listings|
-#       job = {
-#         title: job_listings.css('a').text,
-#         place: count += 1
-#       }
-#       jobs << job
-
-#   end
-#   byebug
+# def empty_validator(title)
+#   title == "" ? true : false
 # end
 
-# scraper
+# while empty_validator(search_type)
+#   puts 'Please, enter the valid command'
+#   search_type = gets.chomp
+# end
+
+def scraper
+  url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page.body)
+  jobs = Array.new
+  job_listings = parsed_page.css('td.titleColumn')
+  count = 0
+  job_listings.each do |job_listings|
+      job = {
+        title: job_listings.css('a').text,
+        place: count += 1
+      }
+      jobs << job
+
+  end
+  byebug
+end
+
+scraper
--- lib/search_engine.rb
+++ lib/search_engine.rb
@@ -1,5 +1,26 @@
+require 'nokogiri'
+require 'httparty'
+require 'byebug'
+
 class SearchEngine
-  def initialize
-    @movies_list = Array.new
-  end
+def initialize 
+   @movie_listings = Array.new
 end
+
+  def parsing_page
+    url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+    unparsed_page = HTTParty.get(url)
+    parsed_page = Nokogiri::HTML(unparsed_page.body)
+    movie_listings = parsed_page.css('td.titleColumn')     
+  end  
+  
+  
+  byebug
+  
+  
+
+
+end
+
+
+
