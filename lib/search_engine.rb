145d229f3f5b583598abd5fba1048acef9bfb713
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-05-12 19:20:55 +0600

    Add place_search method

--- lib/search_engine.rb
+++ lib/search_engine.rb
@@ -1,15 +1,24 @@
 require_relative '../lib/scrapper.rb'
 
 class SearchEngine < Scrapper  
-   def place_search(place)   
+  def title_search(place)   
       result = String.new
       hash_creator.each do |item|
         result = item[:title] if item[:place] == place
       end
       result
-   end
+  end
+
+  def place_search(movie)   
+    result = String.new
+    hash_creator.each do |item|
+      result = item[:place] if item[:title] == movie
+    end
+    result
+  end
+
 end
 
 object1 = SearchEngine.new
-puts object1.place_search(1)
+puts object1.place_search("Babam ve Oglum")
 # hash_creator[0][:place] == 1 => hash_creator[0][:title]
