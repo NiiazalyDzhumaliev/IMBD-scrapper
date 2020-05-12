a5aecffcba5d083f44f5c8eb2a76e9c95d61efd8
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-05-12 15:09:36 +0600

    Add empty string validation method

--- bin/main.rb
+++ bin/main.rb
@@ -8,29 +8,33 @@ puts ''
 puts 'You can find it by TITLE or by PLACE in a ranking'
 puts ''
 print 'Choose 1 if you want search by TITLE and 2 for searching by PLACE:'
-search_type = gets.chomp.to_i
+search_type = gets.chomp
 
-until search_type_validator(search_type)
+def empty_validator(title)
+  title == "" ? true : false
+end
+
+while empty_validator(search_type)
   puts 'Please, enter the valid command'
-  search_type = gets.chomp.to_i
+  search_type = gets.chomp
 end
 
-def scraper
-  url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
-  unparsed_page = HTTParty.get(url)
-  parsed_page = Nokogiri::HTML(unparsed_page.body)
-  jobs = Array.new
-  job_listings = parsed_page.css('td.titleColumn')
-  count = 0
-  job_listings.each do |job_listings|
-      job = {
-        title: job_listings.css('a').text,
-        place: count += 1
-      }
-      jobs << job
+# def scraper
+#   url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+#   unparsed_page = HTTParty.get(url)
+#   parsed_page = Nokogiri::HTML(unparsed_page.body)
+#   jobs = Array.new
+#   job_listings = parsed_page.css('td.titleColumn')
+#   count = 0
+#   job_listings.each do |job_listings|
+#       job = {
+#         title: job_listings.css('a').text,
+#         place: count += 1
+#       }
+#       jobs << job
 
-  end
-  byebug
-end
+#   end
+#   byebug
+# end
 
-scraper
+# scraper
