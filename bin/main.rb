42d5425ca5d43d6da30d0c3d0e7d54f41b741e08
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-05-12 21:12:42 +0600

    Fix rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -7,7 +7,6 @@ require_relative '../lib/search_engine.rb'
 search_engine = SearchEngine.new
 validator = Validator.new
 
-
 puts '                            -----WELCOME------'
 puts 'Here you can find a film from TOP 250 IMBD ranking'
 puts ''
@@ -18,13 +17,13 @@ print 'Choose ---1--- if you want to search by PLACE and ---2--- for searching b
 search_type = gets.chomp
 
 until validator.search_type_validator(search_type)
-    print 'Please, choose ---1--- or ---2--- :  '
-    search_type = gets.chomp
+  print 'Please, choose ---1--- or ---2--- :  '
+  search_type = gets.chomp
 end
 
 if validator.choice_validator(search_type)
   print 'Please, type a place of movie in IMBD ranking: '
-  place_of_choice = gets.chomp.to_i  
+  place_of_choice = gets.chomp.to_i
   puts search_engine.title_search(place_of_choice)
 else
   print 'Please, type a title of movie in IMBD ranking: '
@@ -32,8 +31,3 @@ else
   title_of_choice = search_engine.capitalize_words(title_of_choice)
   puts search_engine.place_search(title_of_choice)
 end
-
-
-
-
-
