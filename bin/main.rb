44289a1b9104846c70900971c8dc4473ffecca72
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-05-12 20:03:26 +0600

    Add search_type validation

--- bin/main.rb
+++ bin/main.rb
@@ -5,29 +5,22 @@ require_relative '../lib/validator.rb'
 require_relative '../lib/search_engine.rb'
 
 search_object = SearchEngine.new
-byebug
-search_object.place_search('The Shawshank Redemption')
+validation_object = Validator.new
 
 
+puts '-----WELCOME------'
+puts 'Here you can find a film from TOP 250 IMBD ranking'
+puts ''
+puts 'You can find it either by TITLE or by PLACE in a ranking'
+puts ''
+print 'Choose ---1--- if you want to search by TITLE and ---2--- for searching by PLACE:  '
 
+search_type = gets.chomp
 
+until validation_object.search_type_validator(search_type)
+    print 'Please, choose ---1--- or ---2--- :  '
+    search_type = gets.chomp
+end
 
-
-# puts '                                     -----WELCOME------'
-# puts 'Here you can find a film from TOP 250 IMBD ranking'
-# puts ''
-# puts 'You can find it by TITLE or by PLACE in a ranking'
-# puts ''
-# print 'Choose 1 if you want search by TITLE and 2 for searching by PLACE:'
-# search_type = gets.chomp
-
-# def empty_validator(title)
-#   title == "" ? true : false
-# end
-
-# while empty_validator(search_type)
-#   puts 'Please, enter the valid command'
-#   search_type = gets.chomp
-# end
 
 
