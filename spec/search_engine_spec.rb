8d09a3e1ce559bf2209baf87c5c231cc3567ac50
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-05-13 15:20:13 +0600

    Add place_search test

--- spec/search_engine_spec.rb
+++ spec/search_engine_spec.rb
@@ -3,17 +3,23 @@ require 'httparty'
 require_relative '../lib/search_engine.rb'
 
 describe SearchEngine do
-  let(:search_engine) {SearchEngine.new}
+  let(:search_engine) { SearchEngine.new }
 
-  context "#capitalize_words" do
-    it "returns capitalized words" do
+  context '#capitalize_words' do
+    it 'returns capitalized words' do
       expect(search_engine.capitalize_words('hello world')).to eql('Hello World')
     end
   end
 
-  context "#title_search" do
-    it "returns the title of the movie" do
+  context '#title_search' do
+    it 'returns the title of the movie' do
       expect(search_engine.title_search(2)).to eql('The Godfather')
+    end
+  end
+
+  context '#place_search' do
+    it 'returns the place of the movie' do
+      expect(search_engine.place_search('The Shawshank Redemption')).to eql(1)
     end
   end
 end
