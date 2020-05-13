95708028f8178ab68ff55961bc9a9d1d37096600
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-05-13 14:58:18 +0600

    Add place_validator method's tests

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -15,4 +15,14 @@ describe Validator do
       expect(validator.search_type_validator('9')).to eql(false)
     end
   end
+
+  context "#place_validator" do
+    it "returns true if the number in (1..250) range" do
+      expect(validator.place_validator(50)).to eql(true)
+    end
+
+    it "returns false if the number is not in (1..250) range" do
+      expect(validator.place_validator(300)).to eql(false)
+    end
+  end
 end
