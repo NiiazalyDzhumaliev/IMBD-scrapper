7e4f6c55140becaffa71736a1ebf374853393c27
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-05-13 15:17:49 +0600

    Add title_search test

--- spec/search_engine_spec.rb
+++ spec/search_engine_spec.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+require 'httparty'
 require_relative '../lib/search_engine.rb'
 
 describe SearchEngine do
@@ -6,6 +8,12 @@ describe SearchEngine do
   context "#capitalize_words" do
     it "returns capitalized words" do
       expect(search_engine.capitalize_words('hello world')).to eql('Hello World')
+    end
+  end
+
+  context "#title_search" do
+    it "returns the title of the movie" do
+      expect(search_engine.title_search(2)).to eql('The Godfather')
     end
   end
 end
